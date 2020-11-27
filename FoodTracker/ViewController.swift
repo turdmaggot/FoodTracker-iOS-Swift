9ab609445b67e00346c8ae25d2cd21f4253420ce
Reiner Tupaz <rstupaz@hotmail.com>
2020-11-28 00:07:08 +0800

    Done initial changes on Main.storyboard, added TextField, Label and Button according to the tutorial.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be created.
+    }
 
 
 }
